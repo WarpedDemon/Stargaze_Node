c213d2c8f0489579650eb0d54d56dd07e1a238e0
EC2 Default User <ec2-user@ip-172-31-17-54.ap-southeast-2.compute.internal>
2022-07-11 12:31:46 +0000

    Port adjustment

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT);
 console.log(colors.magenta("[Server]") + colors.white(": SERVER STARTED"));
 
 
